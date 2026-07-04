1813f2acbda669be12d6a41061e446c9b8fdce2b
laborant <laborant@labs.iximiuz.com>
2026-07-04 13:07:36 +0000

    feat: add tests

---
+++ webui/bff/config.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	GRPCEndpoint string
+	HTTPPort     int
+}
+
+func LoadConfig() *Config {
+	cfg := &Config{
+		GRPCEndpoint: "localhost:50051",
+		HTTPPort:     8080,
+	}
+
+	if ep := os.Getenv("BEEMON_GRPC_ENDPOINT"); ep != "" {
+		cfg.GRPCEndpoint = ep
+	}
+
+	if portStr := os.Getenv("BEEMON_HTTP_PORT"); portStr != "" {
+		if p, err := strconv.Atoi(portStr); err == nil {
+			cfg.HTTPPort = p
+		}
+	}
+
+	return cfg
+}
